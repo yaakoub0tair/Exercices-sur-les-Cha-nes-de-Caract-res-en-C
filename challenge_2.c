20d9b5f4bdbb6421bb3184b83411ffc19d65ac10
Yaakoub tair <yaakoubt05@gmail.com>
2025-09-09 12:34:44 +0200

    challenge 2

---
+++ challenge_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+int mystrlen(char klimat[]){
+    int i=0;
+    while (klimat[i]!='\0')
+    {
+        ++i;
+    }
+
+return i;
+}
+int main(){
+     char klimat[100];
+
+    printf("siaisr une chaine de caractere a afficher :\n");
+    fgets(klimat,sizeof(klimat),stdin);
+    
+    int i=mystrlen(klimat);
+    printf("length |||||||||%d\n",i);
+    printf("length:%d",strlen(klimat));
+
+
+
+
+
+
+
+
+
+    return 0;
+}
